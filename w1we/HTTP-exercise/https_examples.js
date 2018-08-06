d03d5098e3fa024ae5eceb1f757a99764c3f9af5
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-06 01:42:57 +0000

    modified the callback function to only showcase data once

--- w1we/HTTP-exercise/https_examples.js
+++ w1we/HTTP-exercise/https_examples.js
@@ -6,9 +6,19 @@ var options = {
 };
 
 // called by https when the request is made.
+// V1 CB
+// var callback = function(response) {
+//   console.log('In response handler callback!');
+//   console.log('Response: ', response);
+// }
+
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
